38fde55f37b57ec15659fe43cbb4b6ce5268ec46
sambrosia <samuelcwoodruff@gmail.com>
2017-04-21 17:49:32 -0600

    Updated dependencies and converted webpack config to Standard style

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,32 @@
-const path = require("path");
+const path = require('path')
 
 module.exports = {
-    devtool: "source-map",
-    entry: ["./src/index.js"],
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "fae.js",
-        library: "fae",
-        libraryTarget: "umd"
+  devtool: 'source-map',
+  entry: ['./src/index.js'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'fae.js',
+    library: 'fae',
+    libraryTarget: 'umd'
+  },
+  externals: {
+    'eventemitter3': {
+      commonjs: 'eventemitter3',
+      commonjs2: 'eventemitter3',
+      amd: 'eventemitter3'
     },
-    externals: {
-        "eventemitter3": {
-            commonjs: "eventemitter3",
-            commonjs2: "eventemitter3",
-            amd: "eventemitter3"
-        },
-        "pixi-sound": {
-            commonjs: "pixi-sound",
-            commonjs2: "pixi-sound",
-            amd: "pixi-sound"
-        },
-        "pixi.js": {
-            commonjs: "pixi.js",
-            commonjs2: "pixi.js",
-            amd: "pixi.js",
-        }
+    'pixi-sound': {
+      commonjs: 'pixi-sound',
+      commonjs2: 'pixi-sound',
+      amd: 'pixi-sound'
     },
-    node: {
-        fs: "empty"
+    'pixi.js': {
+      commonjs: 'pixi.js',
+      commonjs2: 'pixi.js',
+      amd: 'pixi.js'
     }
-};
+  },
+  node: {
+    fs: 'empty'
+  }
+}
