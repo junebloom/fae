a6b76ec7503e29072469421777dc0a74c03d244a
June Bloom <june.a.bloom@gmail.com>
2020-10-15 19:28:29 -0700

    Replace groups tests with tags tests

--- src/core/Entity.test.js
+++ src/core/Entity.test.js
@@ -60,35 +60,21 @@ test("component parameters are passed to init", (t) => {
   t.is(entity.flavor, "lime", "Flavor should be set to the passed parameter.");
 });
 
-// Groups API tests
-test("is in 'all' group", (t) => {
+// Tags API tests
+test("has 'all' tag", (t) => {
   const app = new Application({ hideBanner: true });
   const entity = new Entity(app);
 
-  t.true(entity.groups.has("all"), "Every entity should be in 'all'");
+  t.true(entity.groups.has("all"), "Every entity should be tagged with 'all'.");
 });
 
-test("can be added to and removed from groups", (t) => {
+test("tags can be added and removed", (t) => {
   const app = new Application({ hideBanner: true });
   const entity = new Entity(app);
 
-  entity.group("cuddly", "whiskers");
-  t.true(entity.groups.has("cuddly"), "Should be very cuddly");
-  t.true(entity.groups.has("whiskers"), "Should have whiskers");
+  entity.tag("cuddly");
+  t.true(entity.tags.has("cuddly"), "Should be very cuddly.");
 
-  entity.ungroup("cuddly", "whiskers");
-  t.false(entity.groups.has("cuddly"), "Should not be cuddly :(");
-  t.false(entity.groups.has("whiskers"), "Should not have whiskers");
-});
-
-test("hasGroups method works", (t) => {
-  const app = new Application({ hideBanner: true });
-  const entity = new Entity(app);
-
-  entity.group("dog", "horse");
-
-  t.true(
-    entity.hasGroups("all", "dog", "horse"),
-    "Should be true if entity is in every group provided"
-  );
+  entity.untag("cuddly");
+  t.false(entity.groups.has("cuddly"), "Should not be cuddly. :(");
 });
