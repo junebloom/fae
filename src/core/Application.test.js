76ee22a857caf5f04adde31c654bb62b825e89fc
June Bloom <june.a.bloom@gmail.com>
2020-10-15 22:12:37 -0700

    Add test implementation

--- src/core/Application.test.js
+++ src/core/Application.test.js
@@ -1,6 +1,22 @@
 import test from "ava";
 import { Application } from "./Application.js";
+import { EventEmitter } from "./EventEmitter.js";
+import { EntityCollection } from "./EntityCollection.js";
+import { SystemManager } from "./SystemManager.js";
 
-// Application API tests
-test.todo("provides the correct interfaces");
+test("provides the correct interfaces", (t) => {
+  const app = new Application({ hideBanner: true });
+
+  t.truthy(app.state, "Should provide a store for global state.");
+  t.true(app.event instanceof EventEmitter, "Should provide an event emitter.");
+  t.true(
+    app.entity instanceof EntityCollection,
+    "Should provide an entity collection."
+  );
+  t.true(
+    app.system instanceof SystemManager,
+    "Should provide a system manager."
+  );
+});
+
 test.todo("can be reset to a blank slate");
