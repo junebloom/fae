7388d943efb2911efabd8d82b74e990d41172ae3
June Bloom <june.a.bloom@gmail.com>
2020-10-15 18:40:08 -0700

    Add EntityCollection implementation

---
+++ src/core/EntityCollection.js
@@ -0,0 +1,74 @@
+import { Entity } from "./Entity.js";
+
+// Represents a set of entities, usually as the result of a query.
+class EntitySet {
+  constructor(collection, entities) {
+    this.collection = collection;
+    this.set = new Set(entities);
+
+    this.forEach = this.set.forEach;
+    this[Symbol.iterator] = this.set.values;
+  }
+
+  // Returns a new set containing only the entities from this set which
+  // have the given tag.
+  and(tag) {
+    return this.filter((entity) => entity.tags.has(tag));
+  }
+
+  // Returns a new set containing only the entities from this set which
+  // do not have the given tag.
+  andNot(tag) {
+    return this.filter((entity) => !entity.tags.has(tag));
+  }
+
+  // Returns a new set containing a union between this set and the set of
+  // entities which have the given tag.
+  or(tag) {
+    return new EntitySet(this.collection, [
+      ...this.set,
+      ...this.collection.get(tag),
+    ]);
+  }
+
+  // Returns a new set filtered in the same way as `Array.prototype.filter`.
+  filter(callback) {
+    const filtered = new EntitySet(this.collection);
+    this.forEach((entity) => {
+      if (callback(entity)) filtered.set.add(entity);
+    });
+    return filtered;
+  }
+}
+
+// Represents the collection of all entities for an app and provides an
+// interface to access them.
+export class EntityCollection {
+  constructor() {
+    // Store internal indexes for quickly accessing entities grouped by tags.
+    this.indexes = new Map();
+  }
+
+  // Returns a new entity.
+  create() {
+    return new Entity(this);
+  }
+
+  // Returns the set of entities for the tag
+  get(tag) {
+    if (this.indexes.has(tag)) return this.indexes.get(tag);
+    else return new EntitySet(this);
+  }
+
+  // Return the requested index, creating it if necessary.
+  index(tag) {
+    let set;
+    if (this.indexes.has(tag)) {
+      set = this.indexes.get(tag);
+    } else {
+      set = new EntitySet(this);
+      this.indexes.set(tag, set);
+    }
+    return set;
+  }
+}
