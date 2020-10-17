0b2790996f5b30b9795285dda99e4372d4f9e977
June Bloom <june.a.bloom@gmail.com>
2020-10-16 19:19:03 -0700

    Improve comment quality in getTime

--- src/utils/getTime.js
+++ src/utils/getTime.js
@@ -1,7 +1,8 @@
-// Return # of seconds past an arbitrary point in time.
+// Return the number of seconds passed since some fixed point in time.
 export function getTime() {
   if (globalThis.performance) return performance.now() / 1000;
   else {
+    // Fall back to a Node API if high-quality standards aren't implemented.
     const t = process.hrtime();
     return t[0] + t[1] / 1e9;
   }
