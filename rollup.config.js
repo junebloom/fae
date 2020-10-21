25bc6043815808f4cb0218290a344e4bda840ab3
June Bloom <june.a.bloom@gmail.com>
2020-10-21 12:02:21 -0700

    Remove unnecessary rollup plugins

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,13 @@
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
 import json from "@rollup/plugin-json";
 import { terser } from "rollup-plugin-terser";
 
 export default {
   input: "src/fae.js",
   output: {
-    file: "dist/fae.js",
-    sourcemap: true,
-    format: "es",
     name: "fae",
+    dir: "dist",
+    format: "es",
+    sourcemap: true,
   },
-  plugins: [resolve(), commonjs(), json(), terser()],
+  plugins: [json(), terser()],
 };
